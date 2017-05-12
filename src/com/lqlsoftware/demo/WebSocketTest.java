6d7ce1c261d19d1252359f2361f32fe81d8c5f8f
unknown <>
2017-05-13 02:49:11 +0800

    init

---
+++ src/com/lqlsoftware/demo/WebSocketTest.java
@@ -0,0 +1,32 @@
+package com.lqlsoftware.demo;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/")
+public class WebSocketTest {
+
+  @OnMessage
+  public void onMessage(String message, Session session) 
+    throws IOException, InterruptedException {
+  
+    // Print the client message for testing purposes
+    System.out.println("Received: " + message);
+    session.getBasicRemote().sendText("Received: " + message);
+  }
+  
+  @OnOpen
+  public void onOpen() {
+    System.out.println("Client connected");
+  }
+
+  @OnClose
+  public void onClose() {
+    System.out.println("Connection closed");
+  }
+}
